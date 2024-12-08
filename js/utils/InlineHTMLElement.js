a87c5b9a05611aea84ad4e65e805faa206f804e9
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2024-12-08 13:50:34 +0200

    feat(inline html): attributes copying

--- js/utils/InlineHTMLElement.js
+++ js/utils/InlineHTMLElement.js
@@ -21,6 +21,20 @@ export default class InlineHTMLElement extends HTMLElement {
     this.#cache.set(src, result);
 
     this.innerHTML = result;
+
+    const rootNode = this.children[0];
+    if (rootNode) {
+      const attributesToCopy = Object.keys(this.dataset).filter((key) =>
+        /^attr[A-Z0-9]/.test(key)
+      );
+      for (const attribute of attributesToCopy) {
+        let attributeName = attribute.replace(/^attr/, "");
+        attributeName =
+          attributeName.charAt(0).toLowerCase() + attributeName.slice(1);
+
+        rootNode.setAttribute(attributeName, this.dataset[attribute]);
+      }
+    }
   }
 }
 
